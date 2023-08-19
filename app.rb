278a1cf1c8cd3ec622b98f34b46e9ca79e024183
KrishY420 <136658616+KrishY420@users.noreply.github.com>
2023-08-19 18:01:17 +0000

    dynamic updates

--- app.rb
+++ app.rb
@@ -5,43 +5,80 @@ get("/") do
   erb(:homepage)
 end
 
+get("/dynamic/:number_of_dice/:number_of_rolls") do
+  @num_dice = params.fetch("number_of_dice").to_i
+  @num_roll = params.fetch("number_of_rolls").to_i
+  @rolls = []
+
+  @num_dice.times do
+    die = rand(1..@num_roll)
+
+    @rolls.push(die)
+  end
+
+  erb(:flexible)
+end
+
 get("/dice/2/6") do
   first_die = rand(1..6)
-	second_die = rand(1..6)
+  second_die = rand(1..6)
   sum = first_die + second_die
-	
-	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-	erb(:two_six)
+
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @first_die = first_die
+  @second_die = second_die
+
+  erb(:two_six)
 end
 
 get("/dice/2/10") do
   first_die = rand(1..10)
-	second_die = rand(1..10)
+  second_die = rand(1..10)
   sum = first_die + second_die
-	
-	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
+
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @first_die = first_die
+  @second_die = second_die
+
   erb(:two_ten)
 end
 
 get("/dice/1/20") do
-  @die = rand(1..20)
-	
-	@outcome = "You rolled a #{@die}."
+  first_die = rand(1..20)
+  second_die = rand(1..20)
+	sum = first_die + second_die
+  
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @first_die = first_die
+  @second_die = second_die
 	
 	erb(:one_twenty)
 end
 
 get("/dice/5/4") do
-  first_die = rand(1..4)
-	second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  fifth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die + fifth_die
-	
-	@outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die} for a total of #{sum}."
-	
+  @rolls = []
+
+  5.times do
+    roll = rand(1..4)
+    @rolls.push(roll)
+  end
+
+  sum = @rolls.sum
+  @outcome = "You rolled #{@rolls.join(', ')} for a total of #{sum}."
+
   erb(:five_four)
+end
+
+get("/dice/50/6") do
+  @rolls = []
+
+  50.times do
+    roll = rand(1..6)
+    @rolls.push(roll)
+  end
+
+  sum = @rolls.sum
+  @outcome = "You rolled #{@rolls.join(', ')} for a total of #{sum}."
+
+  erb(:fifty_six)
 end
